9f0496853a3cf8eb19b1028444b8894cd7aec92a
Julian Kaljuvee <kaljuvee@gmail.com>
2023-10-14 22:00:18 +0300

    Update app.py

--- st/app.py
+++ st/app.py
@@ -8,20 +8,6 @@ st.title('Biotech News Processor')
 
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
-def adjust_dates_for_weekends(today_date):
-    """Adjusts the date for weekends."""
-    if today_date.weekday() == 5:  # Saturday
-        yesterday_date = today_date - timedelta(days=1)
-        tomorrow_date = today_date + timedelta(days=2)
-    elif today_date.weekday() == 6:  # Sunday
-        yesterday_date = today_date - timedelta(days=2)
-        tomorrow_date = today_date + timedelta(days=1)
-    else:
-        yesterday_date = today_date - timedelta(days=1)
-        tomorrow_date = today_date + timedelta(days=1)
-    
-    return yesterday_date, tomorrow_date
-
 if uploaded_file:
     data = pd.read_csv(uploaded_file)
     
@@ -37,21 +23,36 @@ if uploaded_file:
     row = data[data['ticker'] == selected_ticker].iloc[0]  # Fetching row using original ticker
 
     # Determine the 'today' date based on the 'published' column
-    if isinstance(row['published_est'], pd.Timestamp):
-        today_date = row['published_est'].to_pydatetime()
-    else:
-        today_date = datetime.strptime(row['published_est'], '%Y-%m-%d %H:%M:%S.%f %z')
-    
-    # Adjust dates
-    start_date = today_date - timedelta(days=2)
-    end_date = today_date + timedelta(days=2)
-    
-    # Convert dates to the yfinance format
-    yf_start_date = start_date.strftime('%Y-%m-%d')
-    yf_end_date = end_date.strftime('%Y-%m-%d')
-
     try:
-        # Fetch stock data for 5 consecutive days
+        if isinstance(row['published_est'], pd.Timestamp):
+            today_date = row['published_est'].to_pydatetime()
+        else:
+            today_date = datetime.strptime(row['published_est'], '%Y-%m-%d %H:%M:%S.%f %z')
+        
+        st.write(f"Today's Date: {today_date}")  # Debugging Print
+    except Exception as e:
+        st.write(f"Error extracting date: {e}")
+
+    # Adjust dates
+    try:
+        start_date = today_date - timedelta(days=2)
+        end_date = today_date + timedelta(days=2)
+        
+        st.write(f"Start Date: {start_date}, End Date: {end_date}")  # Debugging Print
+    except Exception as e:
+        st.write(f"Error adjusting dates: {e}")
+
+    # Convert dates to the yfinance format
+    try:
+        yf_start_date = start_date.strftime('%Y-%m-%d')
+        yf_end_date = end_date.strftime('%Y-%m-%d')
+        
+        st.write(f"yfinance Start Date: {yf_start_date}, yfinance End Date: {yf_end_date}")  # Debugging Print
+    except Exception as e:
+        st.write(f"Error converting dates for yfinance: {e}")
+
+    # Fetch stock data for 5 consecutive days
+    try:
         stock_data = yf.download(selected_ticker, interval='1d', start=yf_start_date, end=yf_end_date)
         
         # Create the area chart
@@ -62,7 +63,7 @@ if uploaded_file:
         
         st.plotly_chart(fig)
     except Exception as e:
-        st.write(f"Error fetching data: {e}")
+        st.write(f"Error fetching data from yfinance: {e}")
     
     # Display only the specified columns
     columns_to_display = ['hyperlinked_ticker', 'title', 'market', 'published_est', 'subject', 'alpha']
