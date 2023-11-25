cf723424764fafdf6380740d4cf2af97c6e3a6a4
Julian Kaljuvee <kaljuvee@gmail.com>
2023-11-25 22:36:12 +0200

    Update and rename create_news_price.py to news_price.py

--- py/news_price.py
+++ py/news_price.py
@@ -1,18 +1,11 @@
 import pandas as pd
-import time
-from sqlalchemy import create_engine
-from datetime import timedelta
-import datetime
-import holidays
-import yfinance as yf
-from datetime import datetime
 from db_util import engine
+import db_util
 import date_util
-import numpy as np
 import price_util
 
 def main():
-    news_df = db_util.read_new_item()
+    news_df = db_util.read_news_item()
     news_df = price_util.create_returns(news_df)
     # Print the processed DataFrame
     print('Writing to db')
@@ -20,3 +13,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
