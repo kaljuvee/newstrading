a0dc6be5c9281b1f63427cce3ea107e54a88a4a0
Julian Kaljuvee <kaljuvee@gmail.com>
2023-12-12 10:58:25 +0200

    Update Home.py

--- st/Home.py
+++ st/Home.py
@@ -7,82 +7,65 @@ import plotly.graph_objects as go
 import db_util
 import pytz
 import date_util
+import feedparser
+import yaml
+from bs4 import BeautifulSoup
+from datetime import datetime
 
-st.title('News Event Analysis Tool - NEAT')
+st.title('Real-Time Press Release Updates - Biotech')
 
-def load_data():
-    """Load data from CSV file."""
-    #return db_util.get_news_price()
-    return pd.read_csv('st/data/news_prices_biotech.csv')
-    
-def process_data(df):
-    """Process the dataframe for hyperlinks."""
-    df['symbol'] = '<a href="https://finance.yahoo.com/quote/' + df['ticker'] + '" target="_blank">' + df['ticker'] + '</a>'
-    df['linked_title'] = '<a href="' + df['link'] + '" target="_blank">' + df['title'] + '</a>'
-    return df
+# Hardcoded sector
+sector = 'biotech'
 
-def fetch_stock_data(ticker, start_date, end_date):
-    """Fetch stock data using yfinance."""
+def clean_text(raw_html):
+    cleantext = BeautifulSoup(raw_html, "lxml").text
+    return cleantext
+
+def fetch_news(rss_dict):
+    cols = ['ticker', 'title', 'summary', 'published_gmt', 'description', 'link', 'language', 'topic', 'sector']
+    all_news_items = []
+
+    for key, rss_url in rss_dict.items():
+        feed = feedparser.parse(rss_url)
+
+        for newsitem in feed['items']:
+            last_subject = newsitem['tags'][-1]['term'] if 'tags' in newsitem and newsitem['tags'] else None
+            all_news_items.append({
+                'ticker': key,
+                'title': newsitem['title'],
+                'summary': clean_text(newsitem['summary']),
+                'published_gmt': newsitem['published'],
+                'description': clean_text(newsitem['description']),
+                'link': newsitem['link'],
+                'language': newsitem.get('dc_language', None),
+                'topic': last_subject,
+                'sector': sector
+            })
+
+    return pd.DataFrame(all_news_items, columns=cols)
+
+def load_config():
+    config_file = "biotech.yaml"
     try:
-        return yf.download(ticker, interval='1h', start=start_date, end=end_date)
+        with open(config_file, 'r') as file:
+            rss_dict = yaml.safe_load(file)
     except Exception as e:
-        st.error(f"Error fetching data from yfinance: {e}")
-        return pd.DataFrame()
-
-def plot_stock_data(stock_data, today_date, ticker):
-    """Plot stock data using Plotly."""
-    fig = px.area(stock_data, x=stock_data.index, y='Close', title=f'Stock Prices for {ticker}')
-    
-    # Ensure stock_data.index is in datetime format
-    stock_data.index = pd.to_datetime(stock_data.index)
-    
-    # Find the closest date in stock_data.index to yf_today_date
-    time_diff = abs(stock_data.index - today_date)
-    closest_date_idx = pd.Series(time_diff).idxmin()
-    closest_date = stock_data.index[closest_date_idx]
-  
-    price_at_closest_date = stock_data.loc[closest_date, 'Close']
-    fig.add_trace(go.Scatter(x=[closest_date], y=[price_at_closest_date], 
-                             mode='markers', marker=dict(color='red', size=10), 
-                             name='Published Time'))
-    st.plotly_chart(fig)
+        st.error(f"Error loading {config_file}: {e}")
+        return None
+    return rss_dict
 
 def main():
-    # Load and process data
-    data = load_data()
-    data = process_data(data)
-
-    # Dropdown for ticker selection
-    data['selection_key'] = data['ticker'] + ' - ' + data['subject'] + ' - ' + data['published_est']
-    selected_ticker = st.selectbox('Select a ticker:', data['selection_key'].head(10).tolist())
-    row = data.loc[data['selection_key'] == selected_ticker].iloc[0]
-
-    # Extract 'today' date
-    try:
-        if isinstance(row['published_est'], pd.Timestamp):
-            today_date = row['published_est'].to_pydatetime()
-        else:
-            today_date = datetime.strptime(row['published_est'], '%Y-%m-%d %H:%M:%S')
-    except Exception as e:
-        st.error(f"Error extracting date: {e}")
+    st.title("Biotech News Aggregator")
+    rss_dict = load_config()
+    
+    if rss_dict is None:
+        st.error("Failed to load RSS feed configuration.")
         return
 
-    today_date = today_date.replace(tzinfo=pytz.timezone('US/Eastern'))
-    
-    # Adjust date range for yfinance
-    start_date, end_date = date_util.adjust_dates_for_weekends(today_date)
-    yf_start_date = start_date.strftime('%Y-%m-%d')
-    yf_end_date = end_date.strftime('%Y-%m-%d')
-    
-    # Fetch stock data and plot
-    stock_data = fetch_stock_data(row['ticker'], yf_start_date, yf_end_date)
-    if not stock_data.empty:
-        plot_stock_data(stock_data, today_date, row['ticker'])
-    
-    # Display table
-    # Format the 'published_est' column
-    columns_to_display = ['symbol', 'linked_title', 'published_est', 'subject', 'market', 'daily_alpha_pct']
-    st.write(data[columns_to_display].head(10).to_html(escape=False, render_links=True, index=False, justify='left'), unsafe_allow_html=True)
+    news_df = fetch_news(rss_dict)
+    last_updated = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    st.write(f"Last updated: {last_updated}")
+    st.dataframe(news_df)
 
 if __name__ == "__main__":
     main()
