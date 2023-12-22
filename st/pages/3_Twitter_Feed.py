edc6c7b8de82c0c326b11b0cfbd97e94d66c20cd
Julian Kaljuvee <kaljuvee@gmail.com>
2023-12-22 07:51:29 +0200

    Add files via upload

---
+++ st/pages/3_Twitter_Feed.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import tweepy
+import pandas as pd
+from datetime import datetime, timedelta
+import time
+
+# Twitter API credentials
+API_KEY = 'glNppNWssA68ac3NFoNSCg8d0'
+API_SECRET_KEY = 'clJteue2FmAHpBRgHBeQEEXiT985tzI4feoTtEhPeA4TA1gheu'
+ACCESS_TOKEN = '16938181-BrOM6V1LTZL9nvJDPyCZhYkEmQwvXrYj2eeCr9elk'
+ACCESS_TOKEN_SECRET = 'n6VD4NOrn3ACvkWRYy9YsREsNQSsJDAQTdF5zakpZOIwQ'
+
+# Twitter handles to monitor
+TWITTER_HANDLES = ["BiotechWorld", "Biotechnology"]
+
+# Initialize Tweepy
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+def get_latest_tweets(twitter_handles):
+    tweets_data = []
+
+    for handle in twitter_handles:
+        # Fetch latest tweets from the handle
+        tweets = api.user_timeline(screen_name=handle, tweet_mode="extended", count=5, exclude_replies=True)
+        for tweet in tweets:
+            tweets_data.append({
+                "Time": tweet.created_at,
+                "User": handle,
+                "Tweet": tweet.full_text
+            })
+
+    return pd.DataFrame(tweets_data)
+
+def main():
+    st.title("Twitter Feed Aggregator")
+
+    # Fetch and display tweets
+    if st.button('Fetch Latest Tweets'):
+        with st.spinner('Fetching tweets...'):
+            tweets_df = get_latest_tweets(TWITTER_HANDLES)
+            st.write(tweets_df)
+
+    # Auto-update tweets every minute
+    if st.checkbox('Auto Fetch Tweets Every Minute'):
+        while True:
+            time.sleep(60)
+            tweets_df = get_latest_tweets(TWITTER_HANDLES)
+            st.write(tweets_df)
+
+if __name__ == "__main__":
+    main()
