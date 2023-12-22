a5e9c2f38b8ff979d5d67de992deb6fb9912eb3d
Julian Kaljuvee <kaljuvee@gmail.com>
2023-12-22 14:38:32 +0200

    Update Home.py

--- st/Home.py
+++ st/Home.py
@@ -30,46 +30,29 @@ def process_data(df):
     df['title'] = '<a href="' + df['link'] + '" target="_blank">' + df['title'] + '</a>'
     return df
     
-@st.cache_data(ttl=60, show_spinner=False)
 def fetch_news(rss_dict, confidence_df):
-    if 'news_item_cache' not in st.session_state:
-        st.session_state.news_item_cache = {}
-
     cols = ['ticker', 'title', 'published_gmt', 'topic', 'link', 'confidence']
-    new_news_items = []
+    all_news_items = []
 
     for key, rss_url in rss_dict.items():
         feed = feedparser.parse(rss_url)
 
         for newsitem in feed['items']:
-            if newsitem['link'] not in st.session_state.news_item_cache:
-                last_subject = newsitem['tags'][-1]['term'] if 'tags' in newsitem and newsitem['tags'] else None
-                confidence = confidence_df[confidence_df['topic'] == last_subject]['confidence'].iloc[0] if any(confidence_df['topic'] == last_subject) else None
+            last_subject = newsitem['tags'][-1]['term'] if 'tags' in newsitem and newsitem['tags'] else None
+            # Lookup the confidence value based on the topic
+            confidence = confidence_df[confidence_df['topic'] == last_subject]['confidence'].iloc[0] if any(confidence_df['topic'] == last_subject) else None
+            all_news_items.append({
+                'ticker': key,
+                # Creating a hyperlink for the title
+                'title': f"<a href='{newsitem['link']}' target='_blank'>{newsitem['title']}</a>",
+                'published_gmt': newsitem['published'],
+                'topic': last_subject,
+                'link': newsitem['link'],
+                'confidence': confidence
+            })
 
-                news_data = {
-                    'ticker': key,
-                    'title': newsitem['title'],
-                    'published_gmt': newsitem['published'],
-                    'topic': last_subject,
-                    'link': newsitem['link'],
-                    'confidence': confidence
-                }
-
-                new_news_items.append(news_data)
-                st.session_state.news_item_cache[newsitem['link']] = news_data
-
-    # Check if there are new items to add
-    if new_news_items:
-        new_df = pd.DataFrame(new_news_items, columns=cols)
-        if 'news_df' in st.session_state and not st.session_state.news_df.empty:
-            # Combine with existing DataFrame
-            combined_df = pd.concat([st.session_state.news_df, new_df], ignore_index=True)
-            st.session_state.news_df = combined_df
-        else:
-            st.session_state.news_df = new_df
-    # Return either the updated or the existing DataFrame
-    return st.session_state.news_df if 'news_df' in st.session_state else pd.DataFrame(columns=cols)
-
+    df = pd.DataFrame(all_news_items, columns=cols)
+    return df
 
 
 def load_config():
@@ -90,46 +73,42 @@ def load_conf_df():
     return confidence_df
 
 def main():
-    # Load configuration
+    # Load config only once
     if 'rss_dict' not in st.session_state:
         st.session_state.rss_dict = load_config()
 
     if 'confidence_df' not in st.session_state:
         st.session_state.confidence_df = load_conf_df()
-
+    
     if st.session_state.rss_dict is None or st.session_state.confidence_df is None:
         st.error("Failed to load configuration.")
         return
 
-    # Manual update button
+    # Update Button
     if st.button('Update'):
-        with st.spinner('Fetching news...'):
-            st.session_state.news_df = fetch_news(st.session_state.rss_dict, st.session_state.confidence_df)
-            st.session_state.news_df = process_data(st.session_state.news_df)
+        st.session_state.news_df = fetch_news(st.session_state.rss_dict, st.session_state.confidence_df)
 
-    # Toggle for automatic fetching
-    if st.checkbox('Auto Fetch News Every Minute', value=st.session_state.get('auto_fetch', False)):
-        st.session_state['auto_fetch'] = True
-        while st.session_state['auto_fetch']:
-            with st.spinner('Fetching news...'):
-                st.session_state.news_df = fetch_news(st.session_state.rss_dict, st.session_state.confidence_df)
-                st.session_state.news_df = process_data(st.session_state.news_df)
-            time.sleep(180)
-            st.rerun()
-    else:
-        st.session_state['auto_fetch'] = False
+    # Initial fetch or fetch every 5 minutes
+    if 'last_updated' not in st.session_state or time.time() - st.session_state.last_updated > 300:
+        news_df = fetch_news(st.session_state.rss_dict, st.session_state.confidence_df)
+        news_df = process_data(news_df)
+        # Sort by 'published_gmt' in descending order
+        news_df = news_df.sort_values(by='published_gmt', ascending=False)
+        st.session_state.last_updated = time.time()
 
-    # Display news
     last_updated = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     st.write(f"Last updated (GMT): {last_updated}")
+
+    # Display specific columns from the DataFrame
     if 'news_df' in st.session_state:
+  # Convert 'published_gmt' to datetime format and sort
         display_df = st.session_state.news_df.copy()
         display_df['published_gmt'] = pd.to_datetime(display_df['published_gmt'])
-        display_df = display_df.sort_values(by='published_gmt', ascending=False)
+        display_df = display_df.drop(columns=['link']).sort_values(by='published_gmt', ascending=False)
+        # Convert DataFrame to HTML and then use st.markdown to render it
         html = display_df.to_html(escape=False, index=False)
         st.markdown(html, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
-
 
